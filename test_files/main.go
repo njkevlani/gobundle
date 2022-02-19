35475d4cda93f0b932db0de06347d443bdcfd698
Nilesh <njkevlani@gmail.com>
2022-02-19 10:59:44 +0530

    Changes in test file
    Added dependent package in test file itself, no external dependency for
    now.

--- test_files/main.go
+++ test_files/main.go
@@ -9,4 +9,13 @@ import (
 func main() {
 	input := []int{1, 2, 3, 4, 5, 2, 6}
 	fmt.Println("Sum =", algo.Sum(input))
+
+	s := algo.Sum(input)
+	fmt.Println(s)
+
+  empty()
+}
+
+func empty() {
+
 }

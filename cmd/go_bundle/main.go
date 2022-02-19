ddf4dd9426139c27ce1e074d56e0901f717743eb
Nilesh <njkevlani@gmail.com>
2022-02-19 11:00:45 +0530

    wip

--- cmd/go_bundle/main.go
+++ cmd/go_bundle/main.go
@@ -3,12 +3,39 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"go/ast"
 	"go/parser"
 	"go/printer"
 	"go/token"
 	"log"
+	"os"
+	"path"
+	// "golang.org/x/tools/go/ast/astutil"
 )
+
+// func getFuncNameWithPkg() {
+// 				if pkgIdent, ok := selectorExpr.X.(*ast.Ident); ok && pkgIdent.Name == "algo" {
+// 					editedFuName := ast.NewIdent(pkgIdent.Name + "_" + selectorExpr.Sel.Name)
+// 					callExpr.Fun = editedFuName
+// 					funcDefinition := v.getFuncDecl()
+// 					v.res.Decls = append(v.res.Decls, funcDefinition)
+// 				}
+// }
+
+type funcDeclCollectorVisitor struct {
+	funcDeclMap map[string]*ast.FuncDecl
+}
+
+func (fv funcDeclCollectorVisitor) Visit(n ast.Node) ast.Visitor {
+	if n != nil {
+		if funcDecl, ok := n.(*ast.FuncDecl); ok {
+			spew.Dump(funcDecl)
+		}
+	}
+
+	return fv
+}
 
 type visitor struct {
 	// TODO Think if I need pointers, will pointer work correctly in my use case?
@@ -17,62 +44,103 @@ type visitor struct {
 	pv  map[*ast.GenDecl]bool // package variables
 	s   map[*ast.StructType]bool
 	v   map[*ast.Expr]bool
+	fv  *funcDeclCollectorVisitor
 }
 
 func (v visitor) Visit(n ast.Node) ast.Visitor {
-	if n == nil {
-		return v
+	if n != nil {
+		if callExpr, ok := n.(*ast.CallExpr); ok {
+			spew.Dump(callExpr)
+			if selectorExpr, ok := callExpr.Fun.(*ast.SelectorExpr); ok {
+				// TODO: fix it. It will work only for "algo" package.
+				if pkgIdent, ok := selectorExpr.X.(*ast.Ident); ok && pkgIdent.Name == "algo" {
+					editedFuName := ast.NewIdent(pkgIdent.Name + "_" + selectorExpr.Sel.Name)
+					callExpr.Fun = editedFuName
+					funcDefinition := v.getFuncDecl()
+					v.res.Decls = append(v.res.Decls, funcDefinition)
+				}
+			}
+		}
 	}
 
-	if c, ok := n.(*ast.CallExpr); ok {
-		fmt.Println(c)
-	}
-	s, ok := n.(*ast.SelectorExpr)
-	if !ok {
-		return v
-	}
-	x, okX := s.X.(*ast.Ident)
-	if !okX {
-		return v
-	}
-	// sel, okSel := s.Sel.(*ast.Ident)
-	// if !okSel {
-	// 	return v
-	// }
-	if x.Obj == nil {
-		fmt.Printf("need to get func %s in package %s\n", s.Sel, s.X)
-	} else {
-		fmt.Printf("need to get func %s on struct %s\n", s.Sel, s.X)
-	}
 	return v
 }
 
+func (v visitor) getFuncDecl() *ast.FuncDecl {
+	// TODO: Get *ast.Package and do ast.Walk(pkg, v.fv)
+	return nil
+}
+
 func main() {
-	fileName := "./test_files/test.go"
-	res := &ast.File{}
+	if len(os.Args) <= 1 {
+		log.Fatalf("Arguments required")
+	}
+
+	fileName := path.Clean(os.Args[1])
 	inFile, err := parser.ParseFile(token.NewFileSet(), fileName, nil, parser.ParseComments)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var walkFunc *ast.FuncDecl
+	// spew.Dump(inFile)
+
+	// importList := astutil.Imports(token.NewFileSet(), inFile)
+	// spew.Dump(importList)
+
+	// fmt.Println("decls")
+	// spew.Dump(inFile.Decls)
+
+	// var walkFunc *ast.FuncDecl
+
+	// for _, decl := range inFile.Decls {
+	// 	if fn, ok := decl.(*ast.FuncDecl); ok {
+	// 		if fn.Name.Name == "main" {
+	// 			fn.Name.Name = "lorem"
+	// 			// spew.Dump(fn.Body.List)
+	// 		}
+	// 		// TODO: Get callExpr out of this and mainupulate it.
+	// 		for _, statements := range fn.Body.List {
+	// 			if expStatement, ok := statements.(*ast.ExprStmt); ok {
+	// 				if callExpr, ok := expStatement.X.(*ast.CallExpr); ok {
+	// 					fmt.Println("CallExpr:")
+	// 					spew.Dump(callExpr)
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// inFile.Decls = append(inFile.Decls, walkFunc)
+	// fmt.Println(astToString(inFile))
+
+	var mainFunc *ast.FuncDecl
 	for _, f := range inFile.Decls {
 		fn, ok := f.(*ast.FuncDecl)
 		if !ok {
 			continue
 		}
 		if fn.Name.Name == "main" {
-			walkFunc = fn
+			mainFunc = fn
 		}
 	}
-	inFile.Decls = append(inFile.Decls, walkFunc)
-	fmt.Println(astToString(inFile))
 
-	if walkFunc == nil {
+	if mainFunc == nil {
 		log.Fatal("No main function found in file", fileName)
 	}
-	v := visitor{res: res}
-	ast.Walk(v, walkFunc)
+
+	// res := &ast.File{}
+	// res.Decls = append(res.Decls, mainFunc)
+	v := visitor{res: inFile}
+	ast.Walk(v, inFile)
+
+	printConfig := &printer.Config{Mode: printer.TabIndent, Tabwidth: 1}
+
+	var buf bytes.Buffer
+	err = printConfig.Fprint(&buf, token.NewFileSet(), inFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buf.String())
 }
 
 func astToString(inFile ast.Node) string {
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/njkevlani/go_bundle
 
 go 1.17
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	golang.org/x/tools v0.1.9 // indirect
+)

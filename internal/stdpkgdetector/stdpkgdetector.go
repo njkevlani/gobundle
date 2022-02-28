eb537d5f679ac3cdaaa9e97fb405f07a0efc3d24
Nilesh <njkevlani@gmail.com>
2022-02-28 22:54:23 +0530

    Use caching in IsStdPkg
    improved performance from 0.5s -> 0.07s

--- internal/stdpkgdetector/stdpkgdetector.go
+++ internal/stdpkgdetector/stdpkgdetector.go
@@ -1,14 +1,44 @@
 package stdpkgdetector
 
 import (
+	"bytes"
+	"encoding/gob"
 	"log"
+	"os"
 
 	"golang.org/x/tools/go/packages"
 )
 
 var isStdPkg = make(map[string]bool)
+var cacheFile = "/tmp/go_bundle_stdpkgdetector.gob"
 
 func init() {
+	err := readFromCache()
+
+	if err != nil {
+		createNew()
+		writeCache()
+	}
+}
+
+func readFromCache() error {
+	b, err := os.ReadFile(cacheFile)
+	if err != nil {
+		return err
+	}
+
+	d := gob.NewDecoder(bytes.NewBuffer(b))
+
+	// Decoding the serialized data
+	err = d.Decode(&isStdPkg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createNew() {
 	pkgs, err := packages.Load(&packages.Config{Mode: packages.NeedSyntax}, "std")
 
 	if err != nil {
@@ -22,7 +52,25 @@ func init() {
 	}
 }
 
-// isStdPkg returns true if given string is a std package in golang, else false.
+func writeCache() {
+	b := new(bytes.Buffer)
+
+	e := gob.NewEncoder(b)
+
+	// Encoding the map
+	err := e.Encode(isStdPkg)
+	if err != nil {
+		log.Println("[WARN] [stdpkgdetector] failed to write cache")
+	}
+
+	err = os.WriteFile(cacheFile, b.Bytes(), 0644)
+
+	if err != nil {
+		log.Println("[WARN] [stdpkgdetector] failed to write cache")
+	}
+}
+
+// IsStdPkg returns true if given string is a std package in golang, else false.
 func IsStdPkg(funcName string) bool {
 	return isStdPkg[funcName]
 }

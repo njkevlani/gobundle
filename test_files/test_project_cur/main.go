17acf6721e6408c07c2a9d92927c816389989fcb
Nilesh <njkevlani@gmail.com>
2022-04-05 06:26:21 +0530

    Added failing test case

---
+++ test_files/test_project_cur/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type node struct {
+	val int
+	in  []int
+}
+
+func main() {
+	nodeVals := []int{1, 2, 3, 4}
+	nodeIns := [][]int{nil, {1}, {1}, {1}}
+
+	var g []node
+	for i := 0; i < len(nodeVals); i++ {
+		g = append(g, node{nodeVals[i], nodeIns[i]})
+	}
+
+    for _, n := range g {
+        fmt.Printf("%#v\n", n)
+    }
+}

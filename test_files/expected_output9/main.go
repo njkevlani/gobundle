27a5469a9ca3f6acd833a04983a9569a620fa07b
Nilesh Kevlani <njkevlani@gmail.com>
2022-04-06 23:19:10 +0530

    Handled cases where variable is defined with type from the same package (#2)
    * Added failing test case
    
    * Added test case in list, added expected output
    
    * wip
    
    * Handled cases where variable is defined with type from the same package
    
    * nit: remove extra line

---
+++ test_files/expected_output9/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nodeVals := []int{1, 2, 3, 4}
+	nodeIns := [][]int{nil, {1}, {1}, {1}}
+	var g []node
+	for i := 0; i < len(nodeVals); i++ {
+		g = append(g, node{nodeVals[i], nodeIns[i]})
+	}
+	for _, n := range g {
+		fmt.Printf("%#v\n", n)
+	}
+}
+
+type node struct {
+	val int
+	in  []int
+}

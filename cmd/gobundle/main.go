7ddc2d26674a93aa6c402c9b29d5ccb09d649143
Nilesh <njkevlani@gmail.com>
2022-04-17 15:54:16 +0530

    Use flag package in main cmd and add version variable

--- cmd/gobundle/main.go
+++ cmd/gobundle/main.go
@@ -1,34 +1,52 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"fmt"
 	"os"
 	"path"
 
 	"github.com/njkevlani/gobundle/internal/gobundle"
 )
 
+var version = "unknown"
+
+func usage() {
+	fmt.Printf(`version: gobundle-%s
+usage: gobundle [-h] file.go
+
+file.go    path to input go file.
+-h         show this message.
+`, version)
+}
+
 func main() {
-	if len(os.Args) <= 1 {
-		log.Fatalf("Arguments required")
+	flag.Usage = usage
+	flag.Parse()
+
+	posArgs := flag.Args()
+	if len(posArgs) != 1 {
+		fmt.Println("Path to go file is required.")
+		usage()
+		os.Exit(1)
 	}
 
-	fileName := path.Clean(os.Args[1])
+	fileName := path.Clean(posArgs[0])
 
 	goimportedBytes, err := gobundle.GoBundle(fileName)
 
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	goimportedBytes = append([]byte("// Auto generated using https://github.com/njkevlani/gobundle\n"), goimportedBytes...)
 
 	err = os.MkdirAll("./build", os.ModePerm)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	err = os.WriteFile("./build/main.go", goimportedBytes, 0644)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
